498e3b728d0ac6c9f3d5c652ca301079ace4e00b
Jake Anderosn <jacob.colin.anderson@icloud.com>
2020-05-02 17:13:09 -0600

    about me page start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,12 +8,16 @@ import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import {MailOutline, LinkedIn, Instagram} from '@material-ui/icons';
+import {MailOutline, LinkedIn, Instagram, GitHub} from '@material-ui/icons';
+import Dialog from '@material-ui/core/Dialog';
+import Fab from '@material-ui/core/Fab'
+import Card from '@material-ui/core/Card'
+import AboutMe from './Components/AboutMe'
 
 const useStyles = makeStyles({
   appBar: {
     background: "transparent",
-    boxShadow: "none"
+    boxShadow: "none",
   },
   bottomNav: {
     position: "fixed",
@@ -24,6 +29,29 @@ const useStyles = makeStyles({
     width: 30,
     height: 30,
     color: "white"
+  },
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dialog:{
+    backgroundColor: "white",  
+    padding: "15px",
+  },
+  card:{
+    marginTop: "60px",
+    maxWidth: "93%",
+    //backgroundColor: "white",
+    //display: "inline-block",
+    maxHeight: "calc(100vh - 120px)",
+    overflowY: "scroll",
+  },  
+  button:{
+    width: "200px",
+    margin: "10px",
+    color: "white",
+    backgroundColor: "#a7d4db",
   }
 });
 
@@ -31,42 +59,63 @@ function App() {
 
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const [open, setOpen] = React.useState(false)
+  
+  const handleClick = () => {
+    setOpen(!open)
+  }
 
   return (
-    <div>
-      <AppBar 
-        position="fixed" 
-        color="inherit"
-        className={classes.appBar}
-      >
-        <Toolbar>
-            <IconButton 
-              edge="start" 
-              color="inherit" 
-              aria-label="menu" 
-            > 
-                <MenuIcon className={classes.icon} />
-            </IconButton>
-        </Toolbar>
-      </AppBar>
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          if(newValue === 0)
-            window.open("mailto:jacob.colin.anderson@icloud.com")
-          if(newValue === 1)
-            window.open("https://www.linkedin.com/in/jacobcolinanderson/")
-          if(newValue === 2)
-            window.open("https://www.instagram.com/schnitzelvonschwein/")
-          setValue(newValue);
-        }}
-        showLabels
-        className={classes.bottomNav}
-      >
-          <BottomNavigationAction value={0} icon={<MailOutline className={classes.icon}/>} />
-          <BottomNavigationAction value={1} icon={<LinkedIn className={classes.icon}/>} />
-          <BottomNavigationAction value={2} icon={<Instagram className={classes.icon}/>} />
-      </BottomNavigation>
+    <div className={classes.content}>
+      <Router>
+        <AppBar 
+          position="fixed" 
+          color="inherit"
+          className={classes.appBar}
+        >
+          <Toolbar>
+              <IconButton 
+                edge="start" 
+                color="inherit" 
+                aria-label="menu" 
+                onClick={handleClick}
+              > 
+                  <MenuIcon className={classes.icon} />
+              </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Card className={classes.card}>
+            <Route path="/aboutme" component={AboutMe}/>
+        </Card>
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            if(newValue === 0)
+              window.location.href = "mailto:jacob.colin.anderson@icloud.com"
+            if(newValue === 1)
+              window.open("https://www.linkedin.com/in/jacobcolinanderson/")
+            if(newValue === 2)
+              window.open("https://www.instagram.com/schnitzelvonschwein/")
+            if(newValue === 3)
+              window.open("https://github.com/jacobcolinanderson")
+            setValue(newValue);
+          }}
+          showLabels
+          className={classes.bottomNav}
+        >
+            <BottomNavigationAction value={0} icon={<MailOutline className={classes.icon}/>} />
+            <BottomNavigationAction value={1} icon={<LinkedIn className={classes.icon}/>} />
+            <BottomNavigationAction value={2} icon={<Instagram className={classes.icon}/>} />
+            <BottomNavigationAction value={3} icon={<GitHub className={classes.icon}/>} />
+        </BottomNavigation>
+        <Dialog onClose={handleClick} aria-labelledby="simple-dialog-title" open={open}>
+          <Card className={classes.dialog}>
+            <Fab component={Link} onClick={handleClick} to="/aboutme" key={1} variant="extended" className={classes.button}>About Me</Fab>
+            <br/>
+            <Fab component={Link} onClick={handleClick} to="/wxlogger" key={1} variant="contained" size="large" className={classes.button}>Wx Logger</Fab>
+          </Card>
+        </Dialog>
+      </Router>
     </div>
     
   );
